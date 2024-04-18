bf34e4a04f571d49678c6922641e5945683f3bab
Tellys Castro <tellyscastro@gmail.com>
2024-04-18 19:33:13 -0300

    docker container operational php7 + mysql8 + apache

--- config/db.php
+++ config/db.php
@@ -1,18 +1,18 @@
 <?php
 
 return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
+    // 'class' => 'yii\db\Connection',
+    // 'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    // 'username' => 'root',
+    // 'password' => '',
+    // 'charset' => 'utf8',
 
     // // for docker container name = mysql8
-    // 'class' => 'yii\db\Connection',
-    // 'dsn' => 'mysql:host=mysql8;dbname=yii2basic',
-    // 'username' => 'root',
-    // 'password' => 's3cr3t',
-    // 'charset' => 'utf8',
+    'class' => 'yii\db\Connection',
+     'dsn' => 'mysql:host=mysql8;dbname=yii2basic',
+     'username' => 'root',
+     'password' => 's3cr3t',
+     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
